c3cfc751c22c2d5b3b9142fc8b4dee713c390850
GersonPro <gers0nl4r6o2001@gmail.com>
2023-05-12 17:05:01 -0500

    a

--- index.js
+++ index.js
@@ -6,35 +6,40 @@ const app = express();
 const port = 3000;
 
 const configuration = new Configuration({
-  apiKey: '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',
-  username: 'gers0nl4r6o2001@gmail.com',
-  password: 'Zk9c6b12',
-  organization: "org-jmdUuZKZUv1JnLm4P2Vjgri6"
+  apiKey: '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',
+  
 });
 
 const openai = new OpenAIApi(configuration);
 
+
+
 app.get("/", async (req, res) => {
+  const valor = req.query.valor;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "como te llamas?",
     temperature: 0.7,
     max_tokens: 60,
     n: 1,
+    
   });
 
   const chatResponse = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "hola quien eres?" }],
+    messages: [{ role: "user", content: valor }],
   });
 
-  const indexHtml = fs.readFileSync("index.html", "utf8");
+
+  const indexHtml = fs.readFileSync("index.html", "utf8",{})
+  
+
   const htmlResponse = `
     <h1>Respuestas de OpenAI</h1>
     <p>Respuesta de texto-davinci-003: ${response.data.choices[0].text}</p>
-    <p>Respuesta de gpt-3.5-turbo: ${chatResponse.data.choices[0].text}</p>
-  `;
-  res.send(indexHtml.replace("{{RESPUESTA}}", htmlResponse));
+    <p>Respuesta de gpt-3.5-turbo: ${JSON.stringify(chatResponse.data.choices[0].message.content)}</p>
+    `;
+  res.send(indexHtml+htmlResponse );
 });
 
 app.listen(port, () => {
--- index2.js
+++ index2.js
@@ -1,53 +1,5 @@
-const express = require('express');
-
-const app = express();
-const port = 3002;
-const openai = new OpenAIApi(apiKey);
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>OpenAI Query</title>
-  </head>
-  <body>
-    <form id="query-form">
-      <label for="prompt">Query:</label>
-      <input type="text" id="prompt" name="prompt" placeholder="Type your query here">
-      <button type="submit">Submit</button>
-    </form>
-    <div id="answer"></div>
-    <script>
-      const form = document.getElementById('query-form');
-      const answerDiv = document.getElementById('answer');
-      form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const prompt = formData.get('prompt');
-        const response = await fetch('/ask', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ prompt })
-        });
-        const answer = await response.text();
-        answerDiv.innerHTML = answer;
-      });
-    </script>
-  </body>
-  </html>
-  `);
-});
-
-app.post('/ask', async (req, res) => {
+import { getCompletion } from "gpt3";
  
-});
-
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+const result = await getCompletion("Once upon a time, ");
+ console.log(result)
+// etc.
